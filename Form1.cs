98d86d48645a55625b34882f9a9b9de571743559
Cvijander <cvija85@gmail.com>
2023-04-16 03:02:20 +0200

    initial commit

---
+++ Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace Podaci_o_radnicima__.Net_
+{
+    public partial class Form1 : Form
+    {
+        // Provider=Microsoft.ACE.OLEDB.12.0;Data Source="C:\Users\Cvijander\source\repos\Relja napredni kurs\Podaci o radnicima (.Net)\Podaci o radnicima (.Net)\bin\Debug\baza.mdb"
+        private OleDbConnection con = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0;Data Source=C:\Users\Cvijander\source\repos\Relja napredni kurs\Podaci o radnicima (.Net)\Podaci o radnicima (.Net)\bin\Debug\baza.mdb");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PrikaziRadnike();
+        }
+
+        private void PrikaziRadnike()
+        {
+            try
+            {
+                con.Open();
+                string tekstKomande = "select * from Radnik";
+                OleDbCommand komanda = new OleDbCommand(tekstKomande, con);
+                OleDbDataAdapter adapter = new OleDbDataAdapter(komanda);
+                DataTable tabela = new DataTable();
+                adapter.Fill(tabela);
+                dataGridView1.DataSource = tabela;
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sif = int.Parse(txtSifraRadnika.Text);
+                string im = txtImeRadnika.Text;
+                string pr = txtPrezimeRadnika.Text;
+                DateTime dat = (DateTime)dateTimePicker1.Value;
+                int plat = int.Parse(txtPlataRandnika.Text);
+                int prem = int.Parse(txtPremijaRadnika.Text);
+                Radnik noviR = new Radnik(sif, im, pr, dat, plat, prem);
+
+                con.Open();
+                string tekstKomande = "insert into Radnik values(" + noviR.SifraRadnika + " ,'" + noviR.ImeRadnika + "','" + noviR.PrezimeRadnika + "', '" + noviR.DatumZaposlenja.ToString("dd/MM/yyyy") + " ', " + noviR.PlataRadnika + ", " + noviR.PremijaRadnika + ")";
+                OleDbCommand komanda = new OleDbCommand(tekstKomande, con);
+                komanda.ExecuteNonQuery();
+                con.Close();
+                PrikaziRadnike();
+                MessageBox.Show("Novi radnik je uspesno upisan.");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Greska prilikom unosa: " + x.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string tekstKomande = "delete from Radnik where sfRadnik =" + txtSifraRadnika.Text;
+                OleDbCommand komanda = new OleDbCommand(tekstKomande, con);
+                komanda.ExecuteNonQuery();
+                con.Close();
+                PrikaziRadnike();
+                MessageBox.Show("Radnik je uspesno obrisan");
+                txtSifraRadnika.Clear();
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Greska prilikom brisanja radnika " + x.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            txtSifraRadnika.Clear();
+            txtImeRadnika.Clear();
+            txtPrezimeRadnika.Clear();
+            txtPlataRandnika.Clear();
+            txtPremijaRadnika.Clear();
+            dateTimePicker1.Value = DateTime.Now;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int sif = int.Parse(txtSifraRadnika.Text);
+                string im = txtImeRadnika.Text;
+                string pr = txtPrezimeRadnika.Text;
+                DateTime dat = (DateTime)dateTimePicker1.Value;
+                int plat = int.Parse(txtPlataRandnika.Text);
+                int prem = int.Parse(txtPremijaRadnika.Text);
+                Radnik noviR = new Radnik(sif, im, pr, dat, plat, prem);
+                con.Open();
+                string tekstKomande = "update Radnik set Ime='" + noviR.ImeRadnika + "',Prezime='" + noviR.PrezimeRadnika + "',DatumZaposlenja='" + noviR.DatumZaposlenja.ToString("dd/MM/yyyy") + "',Plata=" + noviR.PlataRadnika + ",Premija=" + noviR.PremijaRadnika + " where sfRadnik=" + noviR.SifraRadnika;
+                OleDbCommand komnada = new OleDbCommand(tekstKomande, con);
+                komnada.ExecuteNonQuery();
+                con.Close();
+                PrikaziRadnike();
+                MessageBox.Show("Radnik je uspesno azuriran.");
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Greska prilikom azuriranja " + x.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void txtSifraRadnika_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                con.Open();
+                string tekstKomande = "select * from Radnik where sfRadnik =" + txtSifraRadnika.Text;
+                OleDbCommand komanda = new OleDbCommand(tekstKomande, con);
+                OleDbDataReader citac = komanda.ExecuteReader();
+                if (citac.Read() == true)
+                {
+                    txtImeRadnika.Text = citac[1].ToString();
+                    txtPrezimeRadnika.Text = citac[2].ToString();
+                    dateTimePicker1.Value = (DateTime)citac[3];
+                    txtPlataRandnika.Text = citac[4].ToString();
+                    txtPremijaRadnika.Text = citac[5].ToString();
+                }
+                else
+                {
+                    txtImeRadnika.Clear();
+                    txtPrezimeRadnika.Clear();
+                    txtPlataRandnika.Clear();
+                    txtPremijaRadnika.Clear();
+                    dateTimePicker1.Value = DateTime.Now;
+                }
+            }
+            catch (Exception x)
+            {
+                MessageBox.Show("Greska prilikom prikazivanja radnika  " + x.Message);
+            }
+            finally
+            {
+                if (con.State == ConnectionState.Open)
+                    con.Close();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ Radnik.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Podaci_o_radnicima__.Net_
+{
+    internal class Radnik
+    {
+        private int sifraRadnika;
+        private string imeRadnika;
+        private string prezimeRadnika;
+        private DateTime datumZaposlenja;
+        private int plataRadnika;
+        private int premijaRadnika;
+
+        public Radnik(int sifraRadnika, string imeRadnika, string prezimeRadnika, DateTime datumZaposlenja, int plataRadnika, int premijaRadnika)
+        {
+            this.SifraRadnika = sifraRadnika;
+            this.ImeRadnika = imeRadnika;
+            this.PrezimeRadnika = prezimeRadnika;
+            this.DatumZaposlenja = datumZaposlenja;
+            this.PlataRadnika = plataRadnika;
+            this.PremijaRadnika = premijaRadnika;
+        }
+
+        public int SifraRadnika { get => sifraRadnika; set => sifraRadnika = value; }
+        public string ImeRadnika { get => imeRadnika; set => imeRadnika = value; }
+        public string PrezimeRadnika { get => prezimeRadnika; set => prezimeRadnika = value; }
+        public DateTime DatumZaposlenja { get => datumZaposlenja; set => datumZaposlenja = value; }
+        public int PlataRadnika { get => plataRadnika; set => plataRadnika = value; }
+        public int PremijaRadnika { get => premijaRadnika; set => premijaRadnika = value; }
+    }
+}
